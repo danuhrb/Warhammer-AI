12887a0866b92cb3102fb592b8be2fa54b634249
9823Intern <intern@9823capital.com>
2025-10-09 01:58:54 -0500

    core

--- core.py
+++ core.py
@@ -1,17 +1,41 @@
-from data_model import *
 import numpy as np
+from movement import MOVE_DIRS
+from wh_types import Unit
+from typing import List
+import random
 
-def edge_distance(a_pos, a_r, b_pos, b_r):
-    return max(0.0, np.linalg.norm(a_pos - b_pos) - a_r - b_r)
+def roll_d6() -> int:
+    return random.randint(1, 6)
+def get_distance(pos1: tuple, pos2: tuple) -> float:
+    return ((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2)**0.5
 
-def within_range(shooter, target, weapon) -> bool:
-    return np.linalg.norm(shooter.pos - target.pos) - shooter.base_r <= weapon.range
+def get_targets_in_range(unit1: Unit, squads: List[Unit]) -> list[Unit]:
+    targets = []
+    for other in squads:
+        if other.owner != unit1.owner and other.alive:
+            distance = get_distance(unit1.pos, other.pos)
+            if distance <= unit1.weapons[0].range:
+                targets.append(other)
+    return targets
 
-def wrap_angle(rad):
-    # -> (-pi, pi]
-    return (rad + np.pi) & (2*np.pi) - np.pi
+def resolve_shooting(attacker: Unit, defender: Unit) -> int:
+    """ Handles the Shooting Action From one Model to Another. """
+    hits = 0
+    rolls = [roll_d6() for _ in range(attacker.weapons[0].shots)]
+    for roll in rolls:
+        if roll >= attacker.weapons[0].to_hit:
+            hits += 1
+            break
+    dmg = hits * attacker.weapons[0].damage
+    return dmg
 
-PHASES = [
-    "p0_command","p0_movement","p0_shooting","p0_charge","p0_fight",
-    "p1_command","p1_movement","p1_shooting","p1_charge","p1_fight"
-]
+def resolve_melee(attacker: Unit, defender: Unit) -> int:
+    """ Handles the Melee Action From one Model to Another. """
+    hits = 0
+    rolls = [roll_d6() for _ in range(attacker.weapons[0].shots)]
+    for roll in rolls:
+        if roll >= attacker.weapons[0].to_hit:
+            hits += 1
+            break
+    dmg = hits * attacker.weapons[0].damage
+    return dmg
